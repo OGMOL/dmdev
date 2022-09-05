3d0da85447692e0a390a44a3ac82c3130cbec770
OGMOL <olegmolokov11@gmail.com>
2022-09-05 20:56:21 +0300

    003 MapReduce

---
+++ src/main/java/lesson21/MapReduceExample.java
@@ -0,0 +1,23 @@
+package lesson21;
+
+import java.util.stream.Stream;
+
+public class MapReduceExample {
+
+
+    public static void main(String[] args) {
+        Stream.of(
+                new Student(18, "Ivan"),
+                new Student(23, "Petr"),
+                new Student(34, "Vasya"),
+                new Student(45, "Sveta"),
+                new Student(20, "Katya"),
+                new Student(68, "Den"),
+                new Student(101, "Kira")
+        )
+                .parallel()
+                .map(Student::getAge)
+                .reduce(Integer::sum)
+                .ifPresent(System.out::println);
+    }
+}
